5235c67af037cc415a3fc80695cc40a986ad5161
Eyal Roginski <eyalrog1@gmail.com>
2024-02-04 20:03:28 +0200

    changed it to work better with neovim

--- user/primes.c
+++ user/primes.c
@@ -1,59 +1,48 @@
-#include "kernel/types.h"
-#include "kernel/stat.h"
-#include "user/user.h"
+#include "../kernel/types.h"
+#include "../kernel/stat.h"
+#include "../user/user.h"
 
-void prime_sieve(int fd_in)
-{
-    int prime;
-    int read_result = read(fd_in, &prime, sizeof(int));
+void prime_sieve(int fd_in) {
+  int prime;
+  int read_result = read(fd_in, &prime, sizeof(int));
 
-    if (read_result == 0)
-    {
-        return;
+  if (read_result == 0) {
+    return;
+  }
+
+  printf("prime %d\n", prime);
+
+  int prime_pipe[2];
+  pipe(prime_pipe);
+
+  if (fork() > 0) {
+    int i;
+    while (read(fd_in, &i, sizeof(int)) != 0) {
+      if (i % prime != 0) {
+        write(prime_pipe[1], &i, sizeof(int));
+      }
     }
-
-    printf("prime %d\n", prime);
-
-    int prime_pipe[2];
-    pipe(prime_pipe);
-
-    if (fork() > 0)
-    {
-        int i;
-        while (read(fd_in, &i, sizeof(int)) != 0)
-        {
-            if (i % prime != 0)
-            {
-                write(prime_pipe[1], &i, sizeof(int));
-            }
-        }
-        close(prime_pipe[1]);
-        wait(0);
-    }
-    else
-    {
-        close(prime_pipe[1]);
-        prime_sieve(prime_pipe[0]);
-    }
+    close(prime_pipe[1]);
+    wait(0);
+  } else {
+    close(prime_pipe[1]);
+    prime_sieve(prime_pipe[0]);
+  }
 }
 
-int main(int argc, char  argv[]) {
-    int main_pipe[2];
-    pipe(main_pipe);
+int main(int argc, char *argv[]) {
+  int main_pipe[2];
+  pipe(main_pipe);
 
-    if (fork() > 0)
-    {
-        for (int i = 2; i < 35; ++i)
-        {
-            write(main_pipe[1], &i, sizeof(int));
-        }
-        close(main_pipe[1]);
-        wait(0);
+  if (fork() > 0) {
+    for (int i = 2; i < 35; ++i) {
+      write(main_pipe[1], &i, sizeof(int));
     }
-    else
-    {
-        close(main_pipe[1]);
-        prime_sieve(main_pipe[0]);
-    }
-    return 0;
+    close(main_pipe[1]);
+    wait(0);
+  } else {
+    close(main_pipe[1]);
+    prime_sieve(main_pipe[0]);
+  }
+  return 0;
 }
